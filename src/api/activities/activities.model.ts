9631880625be2d7a4a3c78934c18737891365fbf
Michal3333 <michalrajczak33@gmail.com>
2022-10-19 21:04:56 +0200

    rename model file

--- src/api/activities/activities.model.ts
+++ src/api/activities/activities.model.ts
@@ -1,10 +1,16 @@
 import { z } from 'zod';
+import { Types } from 'mongoose';
 
 const Activity = z.object({
   name: z.string().min(1),
+  comment: z.string().min(1),
   value: z.number().min(1),
 });
 
 type Activity = z.infer<typeof Activity>;
+
+export type ActivityWithId = Activity & {
+  _id: Types.ObjectId
+};
 
 export default Activity;
